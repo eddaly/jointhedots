960078b2ba5edbddb8fb99cfb48565f882762c70
eddaly <edward.daly@gmail.com>
2013-06-23 21:23:11 +0100

    Made the text bigger for mobile

--- Assets/Scripts/Hud.cs
+++ Assets/Scripts/Hud.cs
@@ -4,18 +4,29 @@ using System.Collections;
 public class Hud : MonoBehaviour {
 	
 	// Hud's child objects
-	GameObject scoreCounter;
+	GameObject scoreCounter, exit;
 
 	// Use this for initialization
 	void Start () {
 		
 		scoreCounter = transform.FindChild("ScoreCounter").gameObject;
 		scoreCounter.guiText.material.color = Color.blue;
+		exit = transform.FindChild("Exit").gameObject;
+		exit.guiText.material.color = Color.red;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+		// 'Button' hittests and responses
+		if (Input.GetMouseButtonUp (0))
+		{
+			// Back to previous screen
+			if (exit.guiText.HitTest (Input.mousePosition))
+			{
+				Application.LoadLevel ("startup");
+			}
+		}
 	}
 	
 	// Called by PlayField.Update 
